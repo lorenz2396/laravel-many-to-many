d23fd598e1b77cb36399ace256c3c7dac8add40e
lorenz2396 <lorenzo.favaretto1996@gmail.com>
2023-09-27 18:54:26 +0200

    .

--- routes/web.php
+++ routes/web.php
@@ -26,6 +26,8 @@ Route::middleware(['auth'])
     ->group(function () {      
     Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
     Route::resource('projects', ProjectController::class)->parameters(['projects' => 'project:slug']);
+    Route::resource('types',TypeController::class);
+    Route::resource('technologies',TechnologyController::class);
 });
 
 require __DIR__ . '/auth.php';
